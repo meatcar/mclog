4a5a34a54dd827c554bc37c410351e49c3e19a11
Denys Pavlov <denys.pavlov@gmail.com>
2012-01-12 20:31:52 -0500

    started playing with url id's

--- expr/routes/index.js
+++ expr/routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+/*
+ * GET home page.
+ */
+
+exports.blog = function(req, res){
+  res.render('index', { title: req.params.id })
+};
