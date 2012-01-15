be3de4ae329c96d25dc2c0764e959ceff44ab481
Denys Pavlov <denys.pavlov@gmail.com>
2012-01-15 00:51:14 -0500

    Made log route work as expected.

--- expr/routes/index.js
+++ expr/routes/index.js
@@ -7,3 +7,13 @@ exports.index = function(req, res){
   res.render('index', { title: 'Meatcar\'s Home', log: 'log' })
 };
 
+/*
+ * GET log page. Display a list of logs
+ */
+
+exports.log = function(req, res){
+    if (req.params.id)
+      res.render('log', { title: req.params.id })
+    else    
+      res.render('log', { title: 'no id'})
+};
